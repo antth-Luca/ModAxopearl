d283641a459d9648829e1e7224392f5d4b9b2a62
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2026-01-05 16:11:36 -0300

    Correção para encantar com TechPérola Encantada

--- neoforge-project/Axopearl/src/main/java/io/github/antthluca/axopearl/events/WorldEvents.java
+++ neoforge-project/Axopearl/src/main/java/io/github/antthluca/axopearl/events/WorldEvents.java
@@ -1,81 +1,77 @@
 package io.github.antthluca.axopearl.events;
 
+import java.util.Iterator;
+
+import io.github.antthluca.axopearl.Axopearl;
 import io.github.antthluca.axopearl.init.InitItems;
 import io.github.antthluca.axopearl.utils.EnchantmentUtils;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.AnvilUpdateEvent;
-import net.neoforged.neoforge.event.entity.player.AnvilCraftEvent;
 
+@EventBusSubscriber(modid = Axopearl.MODID)
 public class WorldEvents {
     private static ItemStack returnItem;
     private static boolean refundXp;
 
+    @SubscribeEvent
     public static void onAnvilUpdateEvent(AnvilUpdateEvent event) {
-        returnItem = null;
-        refundXp = false;
-
         ItemStack rightItemStack = event.getRight();
         Item rightItem = rightItemStack.getItem();
         ItemStack leftItemStack = event.getLeft();
         Item leftItem = leftItemStack.getItem();
+        // (->) Is White Pearl
+        if (rightItem == InitItems.ENCHANTED_TECHPEARL.value()) {
+            ItemEnchantments rightEnchants = rightItemStack.get(DataComponents.ENCHANTMENTS);
+            // In case unenchanted White Pearl
+            if (!rightItemStack.isEnchanted()) {
+                event.setCanceled(true);
+                return;
+            }
 
-        Item compWhitePearl = InitItems.WHITE_TECHPEARL.value();
-        
-        boolean changed = false;
-        EnchantmentUtils.EnchantResult finalResult = null;
-        ItemEnchantments leftEnchants = EnchantmentHelper.getEnchantmentsForCrafting(leftItemStack);
-        ItemEnchantments.Mutable leftEnchantsMutable = new ItemEnchantments.Mutable(leftEnchants);
-        ItemEnchantments rightEnchants = EnchantmentHelper.getEnchantmentsForCrafting(rightItemStack);
-
-        if (leftItem == compWhitePearl) {
-            if (rightItemStack.isEnchanted()) {
-                if (rightItem == compWhitePearl) {
-                    // Enchant, but no XP cost
-                    changed = true;
-                    finalResult = EnchantmentUtils.enchantItem(leftItemStack.getItemHolder(), leftEnchantsMutable, rightEnchants);
-                    refundXp = true;
-                    returnItem = new ItemStack(compWhitePearl);
-                } else if (rightItem == Items.ENCHANTED_BOOK) {
-                    // Common enchant, with XP cost
-                    changed = true;
-                    finalResult = EnchantmentUtils.enchantItem(leftItemStack.getItemHolder(), leftEnchantsMutable, rightEnchants);
-                    returnItem = new ItemStack(Items.BOOK);
+            boolean changed = false;
+            ItemEnchantments leftEnchants = EnchantmentHelper.getEnchantmentsForCrafting(leftItemStack);
+            ItemEnchantments.Mutable leftEnchantMutable = new ItemEnchantments.Mutable(leftEnchants);
+            int expCost = EnchantmentUtils.ANVIL_BASE_COST;
+            // (<-) Is any enchantable item
+            if (leftItemStack.isEnchantable()) {
+                // Enchanting the left item
+                Iterator iterator = rightEnchants.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    Object2IntMap.Entry<Holder<Enchantment>> rightEntry = (Object2IntMap.Entry) iterator.next();
+                    Holder<Enchantment> rightKey = (Holder) rightEntry.getKey();
+                    if (EnchantmentUtils.canEnchantPerSupport(leftItemStack.getItemHolder(), rightKey)) {
+                        if (EnchantmentUtils.canEnchantPerCompatibility(leftEnchantMutable, rightKey)) {
+                            int rightLevel = rightEntry.getIntValue();
+                            int leftLevel = leftEnchantMutable.getLevel(rightKey);
+                            int finalLevel = leftLevel == rightLevel ? rightLevel + 1 : Math.max(leftLevel, rightLevel);
+                            if (finalLevel != leftLevel) {
+                                changed = true;
+                                leftEnchantMutable.set(rightKey, finalLevel);
+                                expCost += finalLevel - 1;
+                            }
+                        }
+                    }
                 }
             }
-        } else if (leftItemStack.isEnchantable()) {
-            if (rightItem == compWhitePearl) {
-                // Common enchant, with XP cost
-                changed = true;
-                finalResult = EnchantmentUtils.enchantItem(leftItemStack.getItemHolder(), leftEnchantsMutable, rightEnchants);
-                returnItem = null;
+            // Event
+            if (!changed) {
+                event.setCanceled(true);
+            } else {
+                ItemStack result = leftItemStack.copy();
+                EnchantmentHelper.setEnchantments(result, leftEnchantMutable.toImmutable());
+                event.setOutput(result);
+                event.setMaterialCost(1);
+                event.setXpCost(expCost);
             }
-        }
-
-        if (!changed) {
-            event.setCanceled(true);
-        } else {
-            ItemStack result = leftItemStack.copy();
-            EnchantmentHelper.setEnchantments(result, finalResult.targetEnchs().toImmutable());
-            result.set(DataComponents.REPAIR_COST, 0);
-            event.setOutput(result);
-            event.setMaterialCost(1);
-            event.setXpCost(EnchantmentUtils.ANVIL_BASE_COST + finalResult.xpCost());
-        }
-    }
-
-    public static void onAnvilTake(AnvilCraftEvent.Post event) {
-        Player player = event.getEntity();
-        if (returnItem != null && !returnItem.isEmpty()) {
-            player.addItem(returnItem);
-        }
-        if (refundXp) {
-            player.giveExperienceLevels(event.getMenu().getCost());
         }
     }
 }
